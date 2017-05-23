3f67bd2137aaed18747033155a6801d12870631a
Kai Moschcau <kai.moschcau@sabre.com>
2017-05-23 12:40:49 +0200

    adds missing documentation to AssociationOptions

--- lib/yard-sequel/associations/association_options.rb
+++ lib/yard-sequel/associations/association_options.rb
@@ -31,20 +31,32 @@ module YardSequel
       attr_reader :read_only_option
 
       class << self
+        # @param [YARD::Parser::Ruby::AstNode] ast_node The node to get the
+        #   value from.
+        # @return [String] the option value of the node.
         def value_from_const_path_ref(ast_node)
           ast_node.source
         end
 
+        # (see .value_from_const_path_ref)
         def value_from_label(ast_node)
           ast_node.source.gsub(/:$/, '')
         end
 
+        # @param (see .value_from_const_path_ref)
+        # @raise [OptionValueParseError] If the passed node is not one of the
+        #   parseable types.
+        # @return (see .value_from_const_path_ref)
         def value_from_node(ast_node)
           raise(OptionValueParseError, ast_node) unless
             NODE_TYPES.include? ast_node.type
           send(:"value_from_#{ast_node.type}", ast_node)
         end
 
+        # @param (see .value_from_const_path_ref)
+        # @raise [OptionValueParseError] If the String literal contains String
+        #   interpolation.
+        # @return (see .value_from_const_path_ref)
         def value_from_string_literal(ast_node)
           if ast_node[0].children.any? { |cn| cn.type == :string_embexpr }
             raise(OptionValueParseError.new("Can't parse String interpolation",
@@ -53,19 +65,24 @@ module YardSequel
           ast_node.jump(:tstring_content).source
         end
 
+        # (see .value_from_const_path_ref)
         def value_from_symbol_literal(ast_node)
           ast_node.jump(:ident, :const).source
         end
 
+        # (see .value_from_const_path_ref)
         def value_from_top_const_ref(ast_node)
           ast_node.source
         end
 
+        # (see .value_from_const_path_ref)
         def value_from_var_ref(ast_node)
           ast_node.jump(:const).source
         end
       end
 
+      # @param [AstNodeHash] ast_node_hash The AstNodeHash to get the options
+      #   from.
       def initialize(ast_node_hash)
         ast_node_hash.each do |key, value|
           key_name = self.class.value_from_label key
